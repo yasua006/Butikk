39fd42d21240db64281f74e078b1cbd6c5abbb55
yasua006 <156091445+yasua006@users.noreply.github.com>
2025-10-20 15:46:19 +0200

    Oppdatert
    Har med rapport

--- index.ts
+++ index.ts
@@ -1,16 +1,22 @@
 // hent sqlite3 fra npm biblioteket
-const sqlite3: any = require('sqlite3');
+const sqlite3 = require('sqlite3');
 // hent prompt sync fra npm biblioteket for å lage input listene
 const spør = require('prompt-sync')(); // da får man prompt funksjonen på node
 
 // lag en klasse
 class Klasse {
-    #db; // gjor til privat
+    // gjort til privat
+    #db;
+    #solgt_list: any;
+    #totalt_inntekt: number;
     // lag en constructor for lett navigering, den blir brukt i et sted
     constructor () {
         // lag en ny database kobling
         this.#db = new sqlite3.Database('database.sqlite');
-        
+        // lag et solgt list
+        this.#solgt_list = [];
+        this.#totalt_inntekt = 0;
+
         // håndtere feil for database laging
         if (!this.#db) {
             console.error("Ingen database fant!");
@@ -33,32 +39,48 @@ class Klasse {
             this.#db.run(`${ny_list} Inventar (${id}, navn TEXT, pris REAL, ${antall})`);
             this.#db.run(`${ny_list} Salg (${id}, vare_id REAL, dato TEXT, ${antall})`);
 
-            // etter på, lagre råder
-            this.#lagring_av_råder();
+            console.log("---------- \n");
+
+            // lag en valg input for menu systemet
+            let valg = spør("Vil du lagre en vare til inventar? (ja/nei)")?.toLowerCase();
+
+            switch (valg) {
+                case "ja":
+                    // kjør lagring
+                    this.#lagring_av_råder();
+                // man vil fjerne
+                case "nei":
+                    // kjør fjerning
+                    this.#slett_inventar_råder();
+                // håndtere tom valg (man vil avslutte)
+                default:
+                    console.log("OK!");
+            }
         });
     }
 
     // privat funksjon til lagring av liste råder
     #lagring_av_råder(): void {
-        // lag lageret for inventar (mock)
+        // lag en mock inventar
         const inventar_lager = this.#db.prepare('INSERT INTO Inventar (navn, pris, antall) VALUES (?, ?, ?)');
-        inventar_lager.run('jus', 99, 1);
+        inventar_lager.run('jus', 99, 100);
 
-        // lag lageret for salg (mock)
+        // lag et mock salg
         const salg_lager = this.#db.prepare('INSERT INTO Salg (vare_id, dato, antall) VALUES (?, ?, ?)');
-        salg_lager.run(1585, '05-05-2025', 3);
+        salg_lager.run(1, '2025-05-05', 3);
 
-        // kjøre lagring av mock listene
+        // lagre mock listene
         inventar_lager.finalize();
         salg_lager.finalize();
 
         // lag–lag varer input
-        let lag_varer: string | undefined = String(spør("Vil du lage en vare? (ENTER - nei)")?.toLowerCase());
+        let lag_varer: string | null = spør("Vil du lage en vare? (ENTER - nei) ")?.toLowerCase();
 
+        
         // lag en løkke for å håndtere laging av varer
         while (lag_varer != "ferdig" && lag_varer != "stopp" && lag_varer) { // stopp om man vil ikke fortsette ("ferdig" eller "stopp" eller tom)
             // lag en navn input
-            let navn: string | null = String(spør("Lag navn for varen: ")?.toLowerCase());
+            let navn: string | null = spør("Lag navn for varen: ")?.toLowerCase();
 
             // håndtere ugyldig navn
             if (!navn) {
@@ -86,10 +108,9 @@ class Klasse {
 
             // unngå inventar laging krasjer
             try {
-                // lag varer fra input for inventar
+                // lag varen i inventar
                 const inventar_inp_lager = this.#db.prepare('INSERT INTO Inventar (navn, pris, antall) VALUES (?, ?, ?)');
                 inventar_inp_lager.run(navn, pris, antall);
-                // kjøre lagring av inventar listen
                 inventar_inp_lager.finalize();
             } catch (err) {
                 // stoppe metoden når feil skjer på catch
@@ -100,46 +121,44 @@ class Klasse {
             }
 
             // spør igjen, samtykke
-            lag_varer = spør("Vil du lage en vare? (ENTER - nei)")?.toLowerCase();
+            lag_varer = spør("Vil du lage mer varer? (ENTER - nei) ")?.toLowerCase();
         }
-
-        // man samtykker ikke, vis resultat
-        this.#slett_inventar_råder();
     }
 
     // funksjon for å håndtere fjerning av inventar råder
     #slett_inventar_råder(): void {
         // lag fjern varer input
-        let fjern_varer: string | undefined = String(spør("Vil du fjerne en vare? (ENTER - nei)")?.toLowerCase());
+        let fjern_varer: string | null = spør("Vil du selge en vare? (ENTER - nei) ")?.toLowerCase();
 
-        // fjern navn input
-        let navn: string | undefined = String(spør("Fjern varens navn: ")?.toLowerCase());
-
-        // håndtere ugyldig navn
-        if (!navn) {
-            console.error("Ugyldig navn!");
-            return;
-        }
-
-        // fjern pris input
-        let pris: number | null = Number(spør("Fjern varens pris: "));
-
-        // beskytt pris mot NaN og mot ugyldig pris (0)
-        if (isNaN(pris) || !pris) {
-            console.error("Ugyldig pris!");
-            return;
-        }
-
-        // fjern antall input
-        let antall: number | null = Number(spør("Fjern varens antall: "));
-
-        // beskytt antall mot NaN og mot ugyldig antall (0)
-        if (isNaN(antall) || !antall) {
-            console.error("Ugyldig antall!");
-            return;
-        }
 
         while (fjern_varer != "ferdig" && fjern_varer != "stopp" && fjern_varer) { // stopp om man vil ikke fortsette ("ferdig" eller "stopp" eller tom)
+            // fjern navn input
+            let navn: string | null = spør("Fjern varens navn: ")?.toLowerCase();
+
+            // håndtere ugyldig navn
+            if (!navn) {
+                console.error("Ugyldig navn!");
+                return;
+            }
+
+            // fjern pris input
+            let pris: number | null = Number(spør("Fjern varens pris: "));
+
+            // beskytt pris mot NaN og mot ugyldig pris (0)
+            if (isNaN(pris) || !pris) {
+                console.error("Ugyldig pris!");
+                return;
+            }
+
+            // fjern antall input
+            let antall: number | null = Number(spør("Fjern varens antall: "));
+
+            // beskytt antall mot NaN og mot ugyldig antall (0)
+            if (isNaN(antall) || !antall) {
+                console.error("Ugyldig antall!");
+                return;
+            }
+            
             // unngå fjerning krasjer
             try {
                 // få id-en
@@ -149,28 +168,35 @@ class Klasse {
                     // håndtere ugyldig vare info
                     if (err) {
                         this.#db.close();
-                        throw new Error(`Kunne ikke fjerne varen fra inventar! ${err.message}`);
+                        throw new Error(`Kunne ikke registrere et salg! ${err.message}`);
                     }
 
                     // håndtere ugyldig råd
                     if (!row) {
                         this.#db.close();
-                        console.error("Fant ikke varen for inventar!");
+                        console.error("Ugyldig råd!");
                         return;
                     }
-
-                    // fjern varene for inventar
-                    const inventar_inp_lager = this.#db.prepare('DELETE FROM Inventar WHERE id = ?');
-                    inventar_inp_lager.run(row.id);
 
                     // lag salget
                     const salg_inp_lager = this.#db.prepare('INSERT INTO Salg (vare_id, dato, antall) VALUES (?, ?, ?)');
                     salg_inp_lager.run(row.id, new Date().toISOString(), antall);
 
-                    // fjern fra inventar
-                    inventar_inp_lager.finalize();
+                    // lag salg navnet i solgt listen
+                    this.#solgt_list.push(navn);
+                    // increment salg prisen for å få totalt inntekt
+                    this.#totalt_inntekt += pris;
+
                     // registrer salget
                     salg_inp_lager.finalize();
+
+                    // fjern varen fra inventaren
+                    const inventar_inp_lager = this.#db.prepare('DELETE FROM Inventar WHERE id = ?');
+                    inventar_inp_lager.run(row.id);
+                    inventar_inp_lager.finalize();
+
+                    // man samtykker ikke, vis resultatet til slutt
+                    this.#hent_og_logg_lister();
                 });
             } catch (err) {
                 // stoppe metoden når feil skjer på catch
@@ -181,12 +207,8 @@ class Klasse {
             }
 
             // spør igjen, samtykke
-            fjern_varer = spør("Vil du fjerne en vare? (ENTER - nei)")?.toLowerCase();
+            fjern_varer = spør("Vil du selge mer varer? (ENTER - nei) ")?.toLowerCase();
         }
-
-        // man samtykker ikke, vis resultat
-        this.#hent_og_logg_lister();
-        console.log("OK!");
     }
 
     // privat funksjon til henting av lister for logging
@@ -196,11 +218,8 @@ class Klasse {
             // håndtere ugyldig henting av lister
             if (err) {
                 this.#db.close();
-                throw new Error(`Kunne ikke hente data for inventar (mock)! ${err.message}`);
+                throw new Error(`Kunne ikke hente data for inventar! ${err.message}`);
             }
-
-            // ingen feil, logg resultatet
-            console.log(row);
         });
 
         // hent og logg data for salg
@@ -208,12 +227,13 @@ class Klasse {
             // håndtere ugyldig henting av lister
             if (err) {
                 this.#db.close();
-                throw new Error(`Kunne ikke hente data for salg (mock)! ${err.message}`);
+                throw new Error(`Kunne ikke hente data for salg! ${err.message}`);
             }
-
-            // ingen feil, logg resultatet
-            console.log(row);
         });
+    
+        // ingen feil, logg resultatet
+        console.log("Solgte varer:", this.#solgt_list);
+        console.log("Inntekt:", this.#totalt_inntekt);
     }
 }
 
